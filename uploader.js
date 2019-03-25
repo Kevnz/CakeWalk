56560ea5857130dee3900732756628887154e5ce
Kevin Isom <kevin@isom.nz>
2019-03-26 12:25:52 +1300

    linted and settings

--- uploader.js
+++ uploader.js
@@ -1,19 +1,22 @@
-var cloudinary = require('cloudinary');
-var config = require("xtconf")(); 
-var fs = require('fs');
+var cloudinary = require('cloudinary')
+var config = require('xtconf')()
+var fs = require('fs')
 
-cloudinary.config({ 
-  cloud_name: config.get('cloudinary-name'), 
-  api_key: config.get('cloudinary-key'), 
-  api_secret: config.get('cloudinary-secret')
-});
+cloudinary.config({
+  cloud_name: config.get('cloudinary-name'),
+  api_key: config.get('cloudinary-key'),
+  api_secret: config.get('cloudinary-secret'),,
+})
 
-
-fs.readdir('./gallery', function (err, files) {
-	for (var i = 0; i < files.length; i++) {
-		console.log(files[i]);
-		cloudinary.uploader.upload('./gallery/' + files[i], function(result) { 
-		  console.log(result) 
-		}, { public_id:  files[i] });
-	};
-});
+fs.readdir('./gallery', function(err, files) {
+  for (var i = 0; i < files.length; i++) {
+    console.log(files[i])
+    cloudinary.uploader.upload(
+      './gallery/' + files[i],
+      function(result) {
+        console.log(result)
+      },
+      { public_id: files[i] }
+    )
+  }
+})
